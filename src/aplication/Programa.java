dd416d2aaec08ec374c74fb2a61a4e397439cd35
dvaldrez <dennisvaldrez@hotmail.com>
2020-12-25 18:56:35 -0300

    criacao departaento

---
+++ src/aplication/Programa.java
@@ -0,0 +1,14 @@
+package aplication;
+
+import model.entities.Deparment;
+
+public class Programa {
+	public static void main (String [] args) {
+		
+		Deparment dep = new Deparment(1,"books");
+		System.out.println(dep.toString());
+		
+		
+	}
+
+}
